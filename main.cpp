74714cac3fb9f86460d5983d74f03f02d0be6838
saumpsh <saumpsh@gmail.com>
2020-02-25 16:09:20 -0800

    Changes made to code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include "helper_stuff.h"
+#include <iostream>
+//#include "helper_stuff.h"
 
-const int my_matrix[3][5] = 
+#include "matrix.h"
+
+using namespace std;
+
+
+
+int my_matrix[M_ROWS][N_COLS] = 
 {
     {2, 1, 3, 9,12},
     {2, 4, 5, 11,14},
     {300, 6, 7, 1234,0}
 };
-const int r1=3;
-const int c1=5;
 
-
+// const int r1=3;
+// const int c1=5;
 
 int main()
 {
---
+++ matrix.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+#define M_ROWS 3
+#define N_COLS 5
+
+void transpose(int arr[M_ROWS][N_COLS]);
