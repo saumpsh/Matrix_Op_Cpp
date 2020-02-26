51ab5b0edfaa1a869d5f917e28b4a8b1094d0e96
saumpsh <saumpsh@gmail.com>
2020-02-25 16:53:50 -0800

    .cpp file removed, only header remains

--- main.cpp
+++ main.cpp
@@ -20,6 +20,9 @@ int my_matrix[M_ROWS][N_COLS] =
 
 int main()
 {
-    transpose(my_matrix);
+    //Creates an object of type Matrix
+    Matrix m;
+    
+    m.transpose(my_matrix);
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -4,4 +4,25 @@ using namespace std;
 #define M_ROWS 3
 #define N_COLS 5
 
-void transpose(int arr[M_ROWS][N_COLS]);
+//void transpose(int arr[M_ROWS][N_COLS]);
+
+class Matrix{
+    public:
+    void transpose(int arr[M_ROWS][N_COLS]);
+};
+
+void Matrix::transpose(int arr[M_ROWS][N_COLS])
+{
+        int trans_matrix[N_COLS][M_ROWS];
+    printf( "%d,%d-\n", N_COLS, M_ROWS); 
+    
+    for (int j = 0; j < N_COLS; j++)
+    {
+        for (int i = 0; i < M_ROWS; i++)
+        {
+            trans_matrix[j][i] = arr[i][j] ;
+            printf( "%d,",trans_matrix[j][i]);   
+        }
+        printf( "\n");
+    }
+}
